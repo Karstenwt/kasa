a7f39de59355eb619756bd2f9533c9409dbf9457
Karsten Walraet <karsten.walraet@gmail.com>
2024-10-29 14:06:38 +0100

    suppression classname footer apropos

--- src/pages/Apropos/APropos.jsx
+++ src/pages/Apropos/APropos.jsx
@@ -14,14 +14,22 @@ const APropos = () => {
           régulièrement vérifiées par nos équipes.
         </Collapse>
         <Collapse title="Respect">
-          La bienveillance fait partie des valeurs fondatrices de Kasa.
+          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+          comportement discriminatoire ou de perturbation du voisinage
+          entraînera une exclusion de notre plateforme.
         </Collapse>
         <Collapse title="Service">
-          Nos équipes se tiennent à votre disposition pour vous fournir une
-          expérience parfaite.
+          La qualité du service est au coeur de notre engagement chez Kasa. Nous
+          veillons à ce que chaque interaction, que ce soit avec nos hôtes ou
+          nos locataires, soit empreinte de respect et de bienveillance
         </Collapse>
         <Collapse title="Sécurité">
-          La sécurité est la priorité de Kasa.
+          La sécurité est la priorité de Kasa, Aussi bien pour nos hôtes que
+          pour les voyageurs, chaque logement correspond aux critères de
+          sécurité établis par nos services. En laissant une note aussi bien à
+          l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
+          standards sont bien respéctés.Nous organisons également des ateliers
+          sur la sécurité domestiques de nos hôtes.
         </Collapse>
       </div>
     </div>
